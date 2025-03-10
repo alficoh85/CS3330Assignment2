1c1de9bb7a517f216bd600535ad2a1ddba00db61
WoodySmith2004 <bsmith_04@yahoo.com>
2025-03-10 02:58:39 -0500

    Human and Wizard are done now too

--- src/middleEarth/basicCharacter/Wizard.java
+++ src/middleEarth/basicCharacter/Wizard.java
@@ -1,5 +1,23 @@
 package middleEarth.basicCharacter;
 
-public class Wizard {
+public class Wizard extends characterAbstract{
+	public Wizard(String name, double health, double power) {
+        super(name, health, power);
+    }
 
+    @Override
+    public boolean attack(characterAbstract target) {
+        if (target instanceof Wizard || target instanceof Human) {
+            return false; 
+        }
+
+        double damage = (target instanceof Dwarf) ? this.power * 1.5 : this.power;
+        target.setHealth(target.getHealth() - damage);
+        return true;
+    }
+
+    @Override
+    public String getRace() {
+        return "Wizard";
+    }
 }

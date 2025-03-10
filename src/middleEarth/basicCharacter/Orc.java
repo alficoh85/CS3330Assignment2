30372e819d6f5dbec671cded0f37d02e5da1e12b
WoodySmith2004 <bsmith_04@yahoo.com>
2025-03-10 02:53:09 -0500

    Orc and Dwarf

--- src/middleEarth/basicCharacter/Orc.java
+++ src/middleEarth/basicCharacter/Orc.java
@@ -1,5 +1,23 @@
 package middleEarth.basicCharacter;
 
-public class Orc {
+public class Orc extends characterAbstract{
+	public Orc(String name, double health, double power) {
+        super(name, health, power);
+    }
 
+    @Override
+    public boolean attack(characterAbstract target) {
+        if (target instanceof Orc || target instanceof Elf) {
+            return false; 
+        }
+
+        double damage = (target instanceof Human) ? this.power * 1.5 : this.power;
+        target.setHealth(target.getHealth() - damage);
+        return true;
+    }
+
+    @Override
+    public String getRace() {
+        return "Orc";
+    }
 }

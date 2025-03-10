4837635dcea83d5cd666b40d39113a6f1ef3dc3e
Alex Cohen <alficoh85@gmail.com>
2025-03-10 02:07:53 -0500

    first commit

---
+++ src/middleEarth/characterAbstract.java
@@ -0,0 +1,5 @@
+package middleEarth;
+
+public class characterAbstract {
+
+}
---
+++ src/middleEarth/oopApp.java
@@ -0,0 +1,10 @@
+package middleEarth;
+
+public class oopApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

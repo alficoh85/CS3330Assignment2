86a479ea27167ee7c862b6d3db1641797753320c
WoodySmith2004 <bsmith_04@yahoo.com>
2025-03-10 13:32:37 -0500

    Starting up the manager section

--- src/middleEarth/manager/characterManager.java
+++ src/middleEarth/manager/characterManager.java
@@ -1,5 +1,23 @@
 package middleEarth.manager;
 
 public class characterManager {
-
+	private middleEarthCharacter[] characters;
+	private int sie;
+	
+	public characterManager() {
+		this.characters = new middleEarthCharacter[5];
+		this.size = 0;
+	}
+	
+	public boolean addCharacter(middleEarthCharacter c) {
+		if(size == characters.length) {
+			resizeArray();
+		}
+		characters[size++] = c;
+		return true;
+	}
+	
+	private void resizeArray() {
+		
+	}
 }

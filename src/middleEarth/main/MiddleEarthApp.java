f7adfea4512bde3abdc866f7b47085c9d37bdba4
Alex Cohen <alficoh85@gmail.com>
2025-03-16 03:01:16 -0500

    Added menu options 2, 3, and 4 to MiddleEarthApp

--- src/middleEarth/main/MiddleEarthApp.java
+++ src/middleEarth/main/MiddleEarthApp.java
@@ -13,14 +13,21 @@ public class MiddleEarthApp {
 		int choice = -1;
 		MiddleEarthCouncil council = MiddleEarthCouncil.getInstance();
 		CharacterManager manager = council.getCharacterManager();
-		MiddleEarthCharacter characterToAdd = null;
+		System.out.println("Welcome to the Middle-earth character manager!\n");
 		while (exit) {
-			System.out.println("Choice text: ");
+			System.out.println("Please choose an option.");
+			System.out.println("1. Add a new character");
+			System.out.println("2. View all characters");
+			System.out.println("3. Update an existing character");
+			System.out.println("4. Delete an existing character");
+			System.out.println("5. Execute all characters' attack actions");
+			System.out.println("6. Exit");
 			choice = input.nextInt();
 			if (choice < 1 || choice > 6) {
 				System.out.println("Invalid input.");
 			}
 			if (choice == 1) {
+				MiddleEarthCharacter characterToAdd = null;
 				System.out.println("Please enter the name of the character you want to add: ");
 				String addName = input.nextLine();
 				System.out.println("Please enter the number of the race of the character you would like to add. ");
@@ -65,9 +72,43 @@ public class MiddleEarthApp {
 				}
 			}
 			if (choice == 2) {
-				//To do: add choices 2-5 and update print at the beginning of the main
+				manager.displayAllCharacters();
+			}
+			if (choice == 3) {
+				System.out.println("Please enter the name of the existing character you would like to update: ");
+				String characterNameToUpdate = input.nextLine();
+				System.out.println("Please enter the name you would like to update to (enter current name to keep name as is): ");
+				String updateName = input.nextLine();
+				System.out.println("Please enter the health you would like to update to (enter current name to keep health as is): ");
+				int updateHealth = input.nextInt();
+				System.out.println("Please enter the power you would like to update to (enter current name to keep power as is): ");
+				int updatePower = input.nextInt();
+				MiddleEarthCharacter characterToUpdate = manager.getCharacter(characterNameToUpdate);
+				boolean updateSuccess = manager.updateCharacter(characterToUpdate, updateName, updateHealth, updatePower);
+				if (updateSuccess == false) {
+					System.out.println("Update failed. Please enter the name of a valid character or make sure you made changes.");
+				} 
+				else {
+					System.out.println("Update successful!");
+				}
+			}
+			if (choice == 4) {
+				System.out.println("Please enter the name of the existing character you would like to delete: ");
+				String deleteName = input.nextLine();
+				MiddleEarthCharacter characterToDelete = manager.getCharacter(deleteName);
+				boolean deleteSuccess = manager.deleteCharacter(characterToDelete);
+				if (deleteSuccess == false) {
+					System.out.println("Delete failed. Please enter the name of a valid character.");
+				}
+				else {
+					System.out.println("Delete successful!");
+				}
+			}
+			if (choice == 5) {
+				
 			}
 			if (choice == 6) {
+				System.out.println("Exiting Middle-earth character manager.");
 				exit = false;
 			}
 		}
